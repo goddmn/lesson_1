f0b47b293025c8c736bfbd1902b435735f1f4a2b
goddmn <alymkulov64@gmail.com>
2020-10-14 18:16:10 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num;
+        int num2;
+        int num3;
+        String word = "";
+        num = 15 * 20;
+        num2 = 200;
+        num3 = num + num2;
+        System.out.println(word + num3);
+
+
+        if (num > 100) {
+            System.out.println("True");
+        } else if (num < 100) {
+            System.out.println("False");
+        } else {
+            System.out.println("Nothing");
+        }
+    }
+}
